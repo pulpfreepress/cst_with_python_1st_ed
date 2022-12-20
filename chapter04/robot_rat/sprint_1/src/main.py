07831dfca9e099f0cfae77dcfbbc93bb8ed95861
swodog <rick@pulpfreepress.com>
2022-12-20 10:33:03 -0500

    Completed Sprint 1.

---
+++ chapter04/robot_rat/sprint_1/src/main.py
@@ -0,0 +1,10 @@
+"""Serves as the point of entry to the Robot Rat Application."""
+
+from robotrat_app import RobotRatApp
+
+def main():
+    robot_rat_app = RobotRatApp()
+
+
+if __name__ == '__main__':
+    main()
---
+++ chapter04/robot_rat/sprint_1/src/robotrat_app.py
@@ -0,0 +1,7 @@
+"""Implements the Robot Rat Application."""
+
+class RobotRatApp():
+    """A Remote-Controlled Robot Rat Application."""
+
+    def __init__(self):
+        print('I am Robot Rat! I am alive!')
