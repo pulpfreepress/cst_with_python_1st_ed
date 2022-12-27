7f03ca704b28c5561540b40fed894a10da3a7de0
swodog <rick@pulpfreepress.com>
2022-12-27 11:11:08 -0500

    Worked Sprint 4.

---
+++ chapter04/robot_rat/sprint_3/src/robotrat_app.py
@@ -0,0 +1,83 @@
+"""Implements the Robot Rat Application."""
+import sys
+
+class RobotRatApp():
+    """A Remote-Controlled Robot Rat Application."""
+
+    # Menu Choice Constants
+    _PEN_UP='1'
+    _PEN_DOWN='2'
+    _TURN_RIGHT='3'
+    _TURN_LEFT='4'
+    _MOVE_FORWARD='5'
+    _PRINT_FLOOR='6'
+    _EXIT='7'
+
+    def __init__(self):
+       pass
+
+    def display_menu(self):
+        """Prints menu items to the console."""
+        print('\n\t\tRobot Rat Control Menu\n')
+        print('\t1. Pen Up')
+        print('\t2. Pen Down')
+        print('\t3. Turn Right')
+        print('\t4. Turn Left')
+        print('\t5. Move Forward')
+        print('\t6. Print Floor')
+        print('\t7. Exit')
+
+    def process_menu_choice(self):
+        # Prompt user for input
+        # Assign input string to variable
+        user_input = input('\n\tEnter Command Number: ')
+        # Use first character of input as menu choice
+        menu_choice = user_input[0]
+        if __debug__: 
+            print(f'You entered command number: {menu_choice}')
+        # Is menu_choice valid command?
+        # YES - Execute command
+        # NO - Display error message and try again
+        match menu_choice:
+            case self._PEN_UP: self.set_pen_up()
+            case self._PEN_DOWN: self.set_pen_down()
+            case self._TURN_RIGHT: self.turn_right()
+            case self._TURN_LEFT: self.turn_left()
+            case self._MOVE_FORWARD: self.move_forward()
+            case self._PRINT_FLOOR: self.print_floor()
+            case self._EXIT: sys.exit()
+            case _: self.print_error_message(menu_choice)
+        
+
+    def start_application(self):
+        while True:
+            self.display_menu()
+            self.process_menu_choice()
+
+    def set_pen_up(self):
+        if __debug__:
+            print('set_pen_up() method called...')
+
+    def set_pen_down(self):
+        if __debug__:
+            print('set_pen_down() method called')
+
+    def turn_left(self):
+        if __debug__:
+            print('turn_left() method called...')
+
+    def turn_right(self):
+        if __debug__:
+            print('turn_right() method called...')
+
+    def move_forward(self):
+        if __debug__:
+            print('move_forward() method called...')
+
+    def print_floor(self):
+        if __debug__:
+            print('print_floor() method called')
+
+    def print_error_message(self, menu_choice):
+        print(f'WARNING: {menu_choice} is an invalid command!')
+
