23df47212de5761ef86332a344dac93bda51fd3b
swodog <rick@pulpfreepress.com>
2024-06-09 12:09:22 -0400

    Worked Chapter 16 examples.

---
+++ chapter16/relative_paths/src/main.py
@@ -0,0 +1,18 @@
+"""Demonstrate writing files to relative paths."""
+
+def main():
+	file_name = 'data/data.txt'
+
+	try:
+		with open(file_name, 'w') as f:
+			f.write('Hello World!')
+
+		with open(file_name, 'r') as f:
+			print(f'{f.read()}')
+
+	except (IOError, Exception) as e:
+		print(f'Problem writing file: {e}')
+
+
+if __name__ == '__main__':
+	main()
