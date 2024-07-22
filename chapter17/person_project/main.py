8476ed97c00c53ce4a40d4c0177a5095281e6ab6
swodog <rick@pulpfreepress.com>
2024-07-22 11:45:43 -0400

    Worked Chapter 17 Examples.

--- chapter17/person_project/main.py
+++ chapter17/person_project/main.py
@@ -1,17 +1,18 @@
+"""Demonstrate object instantiation and attribute access."""
 
 from person import Person
 
 def main():
 	p1 = Person()
-	print(f'p1 = {p1} | Object Count: {p1.count}')
+	print(f'p1 = {p1} | Object Count: {Person.count}')
 	p2 = Person('Rick', 'W', 'Miller')
-	print(f'p2 = {p2} | Object Count: {p2.count}')
+	print(f'p2 = {p2} | Object Count: {Person.count}')
 	p3 = Person()
 	p3.first_name = 'Hannah'
 	p3.middle_name = 'J'
 	p3.last_name = 'Bananna'
-	print(f'p3 = {p3} | Object Count: {p3.count}')
-	
+	print(f'p3 = {p3} | Object Count: {Person.count}')
+
 
 if __name__ == '__main__':
 	main()
