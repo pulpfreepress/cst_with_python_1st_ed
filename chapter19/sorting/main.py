8915cb755a05fead6254bfda2b318cb8cbb5f358
swodog <rick@pulpfreepress.com>
2025-07-06 12:30:49 -0400

    Worked chapter 19 sorting example.

--- chapter19/sorting/main.py
+++ chapter19/sorting/main.py
@@ -2,6 +2,7 @@
 
 from person import Person
 from datetime import datetime
+from prettytable import PrettyTable
 
 
 sort_by_last_name = lambda rhs: rhs.last_name
@@ -14,26 +15,23 @@ def main():
 	p4 = Person('Melissa', 'M', 'Groth', datetime(1993, 10, 15))
 	p5 = Person('Kyle', 'V', 'Miller', datetime(1988, 3, 20))
 	people = [p1, p2, p3, p4, p5]
+	sorted_people = sorted(people)
+	sorted_people_descending = sorted(people, reverse=True)
+	ad_hoc_sorted_by_last_name = sorted(people, key=sort_by_last_name)
+	ad_hoc_sorted_by_last_name_descending = \
+			sorted(people, key=sort_by_last_name, reverse=True)
+
+	table = PrettyTable()
+	table.field_names = ["Unsorted", "Age Ascending", "Age Descending", \
+				 "Last Name Ascending", "Last Name Descending"]
 	
-	print('Unsorted:')
-	for p in people:
-		print(f'{p.full_name_and_age}')
+	for i in range(5):
+		table.add_row([people[i], sorted_people[i], sorted_people_descending[i], \
+		ad_hoc_sorted_by_last_name[i], ad_hoc_sorted_by_last_name_descending[i]])
 	
-	print('\nSorted Ascending:')
-	for p in sorted(people):
-		print(f'{p.full_name_and_age}')
+	print(table)
 
-	print('\nSorted Descending (Reversed):')
-	for p in sorted(people, reverse=True):
-		print(f'{p.full_name_and_age}')
-
-	print('\nAd Hoc Sort by Last Name Ascending:')
-	for p in sorted(people, key=sort_by_last_name):
-		print(f'{p.full_name_and_age}')
-
-	print('\nAd Hoc Sorted by Last Name Descending (Reversed):')
-	for p in sorted(people, key=sort_by_last_name, reverse=True):
-		print(f'{p.full_name_and_age}')
-
+	
+	
 if __name__ == '__main__':
 	main()
