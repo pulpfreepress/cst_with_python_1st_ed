f8a9f2eb8c420e6ff8a7f67539f3afc95bc78d04
swodog <rick@pulpfreepress.com>
2025-08-10 07:03:46 -0400

    Cleaned up server code.

--- chapter21/multi_threaded_echo_server/server/src/multi_threaded_echo_server.py
+++ chapter21/multi_threaded_echo_server/server/src/multi_threaded_echo_server.py
@@ -36,7 +36,7 @@ class MultiThreadedEchoServer():
 				self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 			self.server.bind((ip, port))
 			self.server.listen(4)
-			print(f'Listening on IP Address: {self._get_all_ipv4_addresses()}' \
+			print(f'Listening on IP Address(es): {self._get_all_ipv4_addresses()}' \
 		 			f' and Port: {port}')
 			
 		except Exception as e:
@@ -44,6 +44,33 @@ class MultiThreadedEchoServer():
 			self.server.close()
 			sys.exit(0)
 
+
+	# Accept incoming connection
+	def _accept_connection(self)->None:
+		"""Accepts incoming client connections. 
+		Passes client socket object to separate thread for processing.
+		"""
+		client = None
+		client_count = 1
+		try:
+			with self.server as s:
+				while True:
+					print(f'Waiting for incoming client connection...')
+					client, address = s.accept()
+					print(f'Accepted client connection from IP Address: ' \
+		   				f'{address[0]} and port {address[1]}')
+					t = threading.Thread(target=self._process_client_connection, \
+						  args=(client,))
+					t.name = f'Client {client_count}'
+					t.daemon = True
+					t.start()
+					client_count += 1
+					
+		except KeyboardInterrupt:
+			print(f'\n **Server shutdown from the console.**\n')
+		except Exception as e:
+			print(f'Problem in _accept_connection(): {e}')
+		
 
 	# Lists all IPV4 addresses
 	def _get_all_ipv4_addresses(self)->List:
@@ -60,46 +87,23 @@ class MultiThreadedEchoServer():
 						ips.append(ip.ip)
 			return ips
 		except Exception as e:
-			print(f'Problem getting host IPs.')
-
-
-	# Accept incoming connection
-	def _accept_connection(self)->None:
-		"""Accepts incoming client connections. 
-		Passes client socket object to separate thread for processing.
-		"""
-		client_count = 1
-		try:
-			with self.server:
-				while True:
-					print(f'Waiting for incoming client connection...')
-					client, address = self.server.accept()
-					print(f'Accepted client connection from IP Address: ' \
-		   				f'{address[0]} and port {address[1]}')
-					t = threading.Thread(target=self._process_client_connection, \
-						  args=(client,))
-					t.name = f'Client {client_count}'
-					client_count += 1
-					t.start()
-					
-		except Exception as e:
-			print(f'Problem accepting connection: {e}')
+			print(f'Problem in _get_all_ipv4_addresses: {e}')
 
 
 	# Process client connection
 	def _process_client_connection(self, client)->None:
 		"""Processes client connection."""
 		try:
-			with client:
+			with client as c:
 				while True:
-					raw_request = client.recv(1024)
+					raw_request = c.recv(1024)
 					if not raw_request:
 						break
 					request = raw_request.decode('utf-8')
 					print(f'request from {threading.current_thread().name}: ' \
 		   				f'{request}')
-					client.send(bytearray(request, 'utf-8'))
+					c.sendall(bytearray(request, 'utf-8'))
 			print(f'Client {threading.current_thread().name} ' \
 		 			f'disconnected. Goodbye!')
 		except Exception as e: 
-			print(f'Problem processing client connection.')
+			print(f'Problem in _process_client_connection(): {e}')
